fbe9acdea198059944605cfd543311099c738709
Osama Aleem <osamaaleem32@outlook.com>
2021-11-12 14:28:42 +0500

    Add project files.

---
+++ Controllers/AuthorController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using dbConnectionTest.Models;
+
+namespace dbConnectionTest.Controllers
+{
+    public class AuthorController : Controller
+    {
+        MAuthor mAuthor = new MAuthor();
+        // GET: Author
+        public ActionResult Index()
+        {
+            ViewBag.bookTypeList = mAuthor.BookTypeList();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(MAuthor mAuthor1)
+        {
+            ViewBag.bookTypeList = mAuthor1.BookTypeList();
+            mAuthor1.InsertToDb(mAuthor1);
+            return View();
+        }
+    }
+}
---
+++ Models/MAuthor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dbConnectionTest.Models
+{
+    public class MAuthor
+    {
+        public string Name { set; get; }
+        public int Age { set; get; }
+        public char Gender { set; get; }
+        public string BookName { set; get; }
+        public int BookPrice { set; get; }
+        public string BookType { set; get; }
+        public List<SelectListItem> BookTypeList()
+        {
+            List<SelectListItem> bookType = new List<SelectListItem>();
+            bookType.Add(new SelectListItem { Text ="Poetry", Value ="Poetry" });
+            bookType.Add(new SelectListItem { Text = "Science Fiction", Value ="Fiction" });
+            bookType.Add(new SelectListItem { Text = "Short Story", Value = "Story" });
+            return bookType;
+        }
+        public string Publisher { set; get; }
+        public int PublishYear { set; get; }
+        public int InsertToDb(MAuthor mAuthor)
+        {
+            int rowEffected;
+            string dbConnection = @"data source = OSAMA-PC\SQLEXPRESS; initial catalog = TestDB; integrated security = True";
+            SqlConnection con = new SqlConnection(dbConnection);
+            con.Open();
+            string query = "INSERT INTO dbo.author (name, age, gender, bookName, bookPrice, bookType, publisher, publishYear) VALUES('"+mAuthor.Name+"','"+mAuthor.Age+"','"+mAuthor.Gender+"','"+mAuthor.BookName+"','"+mAuthor.BookPrice+"','"+mAuthor.BookType+"','"+mAuthor.Publisher+"','"+mAuthor.PublishYear+"')";
+            SqlCommand cmd = new SqlCommand(query, con);
+            rowEffected = cmd.ExecuteNonQuery();
+            con.Close();
+            return rowEffected;
+        }
+    }
+}
+
+
+
+
+
+
+
